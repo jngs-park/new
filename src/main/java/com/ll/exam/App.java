9fd65cc1f16c4d437a39f183c45445d67becb799
jngs-park <dear.jayp@gmail.com>
2024-09-18 15:28:14 +0900

    입력받은 명언을 리스트에 저장, 목록 기능 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,10 +1,15 @@
 package com.ll.exam;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public void run() {
 
+        // 가장 마지막 명언글의 번호
+        List<WiseSaying> wiseSayings = new ArrayList<>();
         int wiseSayinglastId = 0;
 
         System.out.println("명언 ssg");
@@ -27,8 +32,17 @@ public class App {
                     int id = ++wiseSayinglastId; //등록시 글 번호 증가
 
                     WiseSaying wiseSaying = new WiseSaying(id, content, author);
-                    System.out.println(wiseSaying);
-                    System.out.printf("%d번 명언이 등록되었습니다. \n", id);
+                    wiseSayings.add(wiseSaying);
+
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    break;
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("-------------------");
+                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+                        WiseSaying wiseSaying_ = wiseSayings.get(i);
+                        System.out.printf("%d / %s / %s\n", wiseSaying_.id, wiseSaying_.content, wiseSaying_.author);
+                    }
                     break;
 
                 case "종료":
