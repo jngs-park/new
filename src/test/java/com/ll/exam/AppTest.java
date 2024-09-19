8921bf18eb53351a488e14f70bc652a317c4923f
jngs-park <dear.jayp@gmail.com>
2024-09-19 15:53:36 +0900

    Url 처리기, Rq 클래스 기능 구현 Beta

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -9,11 +9,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
-        public void 테스트_실험(){
-            int rs = 10 + 20;
-            assertEquals(30, rs);
-        }
-        @Test
+    public void 테스트_실험() {
+        int rs = 10 + 20;
+        assertEquals(30, rs);
+    }
+
+    @Test
     public void 문자열을_스캐너의_입력으로_설정() {
         String input = """
                 등록
@@ -30,24 +31,22 @@ public class AppTest {
         assertEquals("등록", cmd);
         assertEquals("명언1", content);
         assertEquals("작가1", author);
-        }
+    }
 
     @Test
-    public void 표준출력을_리다이렉션하여_결과를_문자열로_받기() throws IOException {
-        // 표준출력을 리다이렉션
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(output));
-
-        System.out.println("안녕");
-
-        String rs = output.toString().trim();
-
-        // 표준출력을 원상복구
-        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
-        output.close();
-
-        assertEquals("안녕", rs);
-        }
+    public void Rq__getIntParam() {
+        Rq rq = new Rq("삭제?id=1");
+        int id = rq.getIntParam("id", 0);
+        assertEquals(1, id);
     }
+    @Test
+    public void Rq__getIntParam__2() {
+        Rq rq = new Rq("검색?id=10&no=1");
+        int id = rq.getIntParam("id", 0);
+        int no = rq.getIntParam("no", 0);
+        assertEquals(10, id);
+        assertEquals(1, no);
+    }
+}
 
 
